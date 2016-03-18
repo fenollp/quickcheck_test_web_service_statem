edc1fe3b0ff1e949984d5783fa6276f293f2fd94
Pierre Fenoll <pierrefenoll@gmail.com>
2016-03-18 16:27:03 +0100

    service works

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 all:
 	rebar3 compile
 
-debug: all
-	rebar3 shell
+debug:
+	rebar3 do compile,shell
 
 clean:
 	rebar3 clean
--- src/mylib_stateful_service.erl
+++ src/mylib_stateful_service.erl
@@ -7,10 +7,7 @@
 %% mylib_stateful_service: simple TCP server.
 
 -export([start_link/0]).
--export([ reset/0
-        , take/0
-        , read/0
-        ]).
+-export([read/0]).
 
 %% gen_server callbacks
 -export([init/1
@@ -24,20 +21,13 @@
 -define(SERVER, ?MODULE).
 
 -record(state, { mr_county_guy = 0 :: non_neg_integer()
-               , socket :: gen_tcp:socket()
+               , listen_socket :: gen_tcp:socket()
                }).
 
 %% API
 
 start_link() ->
-    io:format("~s\n", [?MODULE]),
     gen_server:start_link({local,?SERVER}, ?MODULE, [], []).
-
-reset() ->
-    gen_server:call(?SERVER, reset).
-
-take() ->
-    gen_server:call(?SERVER, take).
 
 read() ->
     gen_server:call(?SERVER, read).
@@ -48,11 +38,12 @@ init([]) ->
     {ok, ListenSocket} = gen_tcp:listen(4000, [ binary
                                               , {packet, 0}
                                               , {reuseaddr, true}
+                                              , {keepalive, false}
                                               , {active, true}
                                               ]),
     io:format("~s listening on port 4000\n", [?SERVER]),
-    {ok, Socket} = gen_tcp:accept(ListenSocket),
-    State = #state{socket = Socket},
+    State = #state{listen_socket = ListenSocket},
+    accept(),
     {ok, State}.
 
 handle_call(read, _From, State) ->
@@ -62,19 +53,32 @@ handle_call(_Request, _From, State) ->
     io:format("unhandled call: ~p\n", [_Request]),
     {stop, not_implemented, {error,not_implemented}, State}.
 
+handle_cast(accept, State=#state{listen_socket = ListenSocket}) ->
+    case gen_tcp:accept(ListenSocket, 100) of
+        {ok, _Socket} -> ok;
+        {error, timeout} -> accept();
+        {error, closed} -> accept()
+    end,
+    {noreply, State};
 handle_cast(_Msg, State) ->
     io:format("unhandled cast: ~p\n", [_Msg]),
     {stop, {error,not_matched}, State}.
 
-%% handle_info(reset, _From, State) ->
-%%     OldValue = State#state.mr_county_guy,
-%%     NewState = State#state{mr_county_guy = 0},
-%%     {reply, OldValue, NewState};
-%% handle_call(take, _From, State) ->
-%%     OldValue = State#state.mr_county_guy,
-%%     NewState = State#state{mr_county_guy = 1 + OldValue},
-%%     {reply, OldValue, NewState};
-%gen_tcp:send(Socket, Response)
+handle_info({tcp,Socket,<<"GET /reset ",_/binary>>}, State) ->
+    OldValue = State#state.mr_county_guy,
+    NewState = State#state{mr_county_guy = 0},
+    io:format("State: ~p\n", [NewState#state.mr_county_guy]),
+    reply(Socket, OldValue),
+    {noreply, NewState};
+handle_info({tcp,Socket,<<"GET /take ",_/binary>>}, State) ->
+    OldValue = State#state.mr_county_guy,
+    NewState = State#state{mr_county_guy = 1 + OldValue},
+    io:format("State: ~p\n", [NewState#state.mr_county_guy]),
+    reply(Socket, OldValue),
+    {noreply, NewState};
+handle_info({tcp_closed,_Socket}, State) ->
+    accept(),
+    {noreply, State};
 handle_info(_Info, State) ->
     io:format("unhandled info: ~p\n", [_Info]),
     {stop, {error,not_matched}, State}.
@@ -83,8 +87,24 @@ terminate(_Reason, _State) ->
     io:format("terminating with ~p: ~p\n", [_State,_Reason]).
 
 code_change(_OldVsn, State, _Extra) ->
-    {'ok', State}.
+    {ok, State}.
 
 %% Internals
+
+accept() ->
+    gen_server:cast(self(), accept).
+
+reply(Socket, Int) ->
+    Data = data(integer_to_binary(Int)),
+    ok = gen_tcp:send(Socket, Data).
+
+data(Bin) ->
+    Payload = ["{\"data\": ", Bin, "}"],
+    [ "HTTP/1.1 200 OK\r\n"
+      "Content-Length: ", integer_to_binary(iolist_size(Payload)), "\r\n"
+      "Content-Type: application/json\r\n"
+      "\r\n"
+    , Payload, "\r\n"
+    ].
 
 %% End of Module.
