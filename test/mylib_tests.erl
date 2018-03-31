9f8e772b47c5d94da9093df4a77cca2f64897775
Pierre Fenoll <pierrefenoll@gmail.com>
2018-03-31 18:31:30 +0200

    master: concuerror?

--- test/mylib_tests.erl
+++ test/mylib_tests.erl
@@ -1,16 +1,18 @@
 %% Copyright © 2018 Pierre Fenoll ‹pierrefenoll@gmail.com›
 %% See LICENSE for licensing information.
 %% -*- coding: utf-8 -*-
--module(mylib_app_tests).
+-module(mylib_tests).
 
 %% Tests to play with Concuerror.
 -include_lib("eunit/include/eunit.hrl").
 -include("mylib.hrl").
 
 play_test() ->
-    {ok,_} = application:ensure_all_started(?APP),
+    %% {ok,_} = application:ensure_all_started(?APP),
+    {ok,Pid} = ?STATEFUL_SERVICE:start_link(),
     1 = ?APP:patch(?BASE "/take"),
     0 = ?APP:patch(?BASE "/reset"),
     1 = ?APP:patch(?BASE "/take"),
     2 = ?APP:patch(?BASE "/take"),
-    ok = application:stop(?APP).
+    %% ok = application:stop(?APP).
+    ok = gen_server:stop(Pid).
